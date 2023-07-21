604d87aead5894a95505327db2b1f50fc6aa75b7
JohnnyRuss <georgiaspontoeli123@gmail.com>
2023-07-21 18:34:15 +0400

    handle saveNav route

--- src/controllers/Navigation/navigationController.ts
+++ src/controllers/Navigation/navigationController.ts
@@ -12,7 +12,31 @@ export const getNavigation = Async(async function (req, res, next) {
   )
     queryObj.category = category;
 
-  const docs = await Nav.find(queryObj);
+  const docs = await Nav.find(queryObj).populate({ path: "blocks.routes" });
 
   res.status(200).json(docs);
+});
+
+export const saveNav = Async(async function (req, res, next) {
+  const body = req.body;
+
+  await Promise.all(
+    Object.keys(body).map(async (key) => {
+      await Nav.findOneAndUpdate(
+        { category: key },
+        {
+          $set: {
+            blocks: body[key].blocks.map((block: any) => ({
+              title: block.title,
+              routes: block.routes,
+            })),
+          },
+        }
+      );
+    })
+  );
+
+  const nav = await Nav.find().populate({ path: "blocks.routes" });
+
+  res.status(201).json(nav);
 });
--- src/routes/Navigation/navigationRoutes.ts
+++ src/routes/Navigation/navigationRoutes.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import { navigationController } from "../../controllers/Navigation";
+import { checkAuth, restrictByRoles } from "../../middlewares";
 
 const router = Router();
 
-router.route("/").get(navigationController.getNavigation);
+router
+  .route("/")
+  .get(navigationController.getNavigation)
+  .put(checkAuth, restrictByRoles(["ADMIN"]), navigationController.saveNav);
 
 export default router;
